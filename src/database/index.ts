643f6724442e12bf9cb553d4a0fe7a44742af8ad
Alex <aleksandrekiknadzee@gmail.com>
2024-09-10 20:10:25 +0400

    feat: add omit to the prisma client

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,11 @@
 import {PrismaClient} from "@prisma/client";
 
-const db = new PrismaClient();
+const db = new PrismaClient({
+  omit: {
+    user: {
+      password: true,
+    },
+  },
+});
 
 export default db;

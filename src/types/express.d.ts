9e8e90cc486096d87da5b903dd4a3d6a93f8c700
Alex <aleksandrekiknadzee@gmail.com>
2024-09-10 15:43:01 +0400

    feat: setup typescript custom type extensions

---
+++ src/types/express.d.ts
@@ -0,0 +1,10 @@
+import {Express} from "express-serve-static-core";
+import {Request} from "express";
+
+declare module "express-serve-static-core" {
+  interface Request {
+    context?: {
+      models?: object;
+    };
+  }
+}

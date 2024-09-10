52484515db26d76b5b1951a36085929a64fc3463
Alex <aleksandrekiknadzee@gmail.com>
2024-09-10 15:45:13 +0400

    feat: setup context adding middleware

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express, {Express, Request, Response} from "express";
 import "dotenv/config";
 import routes from "./routes";
+import models from "./models";
 
 const app = express();
 
+app.use((req, res, next) => {
+  req.context = {
+    models,
+  };
+  next();
+});
 app.use("/users", routes.user);
-
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });

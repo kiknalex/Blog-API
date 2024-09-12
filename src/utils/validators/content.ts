338f2b19d8ad7141f7fb4d0862622f49d714d69f
Alex <aleksandrekiknadzee@gmail.com>
2024-09-12 18:08:58 +0400

    feat: add post edit validation rules

---
+++ src/utils/validators/content.ts
@@ -0,0 +1,34 @@
+import {body, oneOf} from "express-validator";
+
+export const validatePost = [
+  body("title")
+    .notEmpty()
+    .withMessage("Title must not be empty.")
+    .isLength({max: 50})
+    .withMessage("Title must be under 50 characters.")
+    .trim()
+    .escape(),
+  body("content")
+    .notEmpty()
+    .withMessage("Content must not be empty.")
+    .isLength({max: 1000})
+    .withMessage("Content must be under 1000 characters.")
+    .trim()
+    .escape(),
+];
+
+export const validateEditPost = [
+  oneOf([body("title").notEmpty(), body("content").notEmpty()]),
+  body("title")
+    .optional({values: "undefined"})
+    .isLength({min: 1, max: 50})
+    .withMessage("Title must be between 1 to 50 characters.")
+    .trim()
+    .escape(),
+  body("content")
+    .optional({values: "undefined"})
+    .isLength({min: 1, max: 1000})
+    .withMessage("Content must be between 1 to 1000 characters.")
+    .trim()
+    .escape(),
+];

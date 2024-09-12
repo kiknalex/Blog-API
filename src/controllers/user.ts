f78b2c6b97a767376d11c8aaab243ab59d85533b
Alex <aleksandrekiknadzee@gmail.com>
2024-09-12 18:22:08 +0400

    fix: incorrect types on function arguments

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,21 +1,19 @@
 import {NextFunction, Request, Response} from "express";
 import models from "@/models";
-import {MiddlewareArguments} from "@/types/middleware";
 
 const user = {
-  getAll: async ({req, res, next}: MiddlewareArguments) => {
+  getAll: async (req: Request, res: Response, next: NextFunction) => {
     try {
       const users = await models.user.getAll();
-      console.log(users);
       res.json(users);
     } catch (error) {
       next(error);
     }
   },
-  getById: async ({req, res, next}: MiddlewareArguments) => {
-    const id = 1;
+  getById: async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
     try {
-      const user = await models.user.getById(id);
+      const user = await models.user.getById(+id);
       res.json(user);
     } catch (error) {
       next(error);

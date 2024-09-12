8161cb888ab75d7833884afbe2262bdaa443e103
Alex <aleksandrekiknadzee@gmail.com>
2024-09-12 18:26:32 +0400

    chore: remove ts compiled files from git tracking.

--- dist/controllers/user.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const user = {
-    getUsers: (req, res) => {
-        res.json({ user: "kiknalex", posts: "lala" });
-    },
-};
-exports.default = user;

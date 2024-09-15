0d1a7717ae0e3f04f0d684d67ebb6da9bd556f6e
Alex <aleksandrekiknadzee@gmail.com>
2024-09-15 20:36:24 +0400

    fix: remove extra slash from origin in corsOptions

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import cors from "cors";
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173/",
+  origin: "http://localhost:5173",
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));

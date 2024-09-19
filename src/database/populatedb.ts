2742374d92afbd1b9346e3d5e46b686492abd6a8
Alex <aleksandrekiknadzee@gmail.com>
2024-09-19 20:10:35 +0400

    refactor: populatedb under faker library

--- src/database/populatedb.ts
+++ src/database/populatedb.ts
@@ -1,42 +1,59 @@
 import db from ".";
-
-const postTitle = "Lorem Ipsum Title";
-const postContent =
-  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic";
-
-const commentContent = "Wow such an inspiring post!";
+import {faker} from "@faker-js/faker";
 
 const userId = 1000;
 const username = "testuser";
 const password = "$2a$10$SEcUwUMcFOZaWSODFDFhJeGVsXbdFlb1IL1Ai4qsmmoOZwr5Z9xnS"; // 12345678
 
+const userIds = [1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010];
+const createRandomUser = (id: number) => {
+  const username = faker.internet.displayName();
+  return {
+    username,
+    password,
+    id,
+  };
+};
+const createRandomPost = (id: number, authorId: number) => {
+  const title = faker.lorem.lines({min: 1, max: 3});
+  const content = faker.lorem.paragraphs({min: 5, max: 10});
+
+  return {
+    id,
+    title,
+    content,
+    authorId,
+  };
+};
+
+const createRandomComment = (postId: number, authorId: number) => {
+  const content = faker.lorem.paragraph({min: 1, max: 10});
+
+  return {
+    content,
+    postId,
+    authorId,
+  };
+};
 const main = async () => {
   console.log("Populating DB...");
   try {
-    await db.user.create({
-      data: {
-        username: username,
-        password: password,
-        id: userId,
-      },
-    });
-    for (let i = 0; i < 3; i++) {
-      await db.post.create({
-        data: {
-          id: i + 1,
-          title: postTitle,
-          content: postContent,
-          authorId: userId,
-        },
+    for (let i = 1001; i < 1010; i++) {
+      const createdUser = await db.user.create({
+        data: createRandomUser(i),
       });
-      for (let j = 0; j < 3; j++) {
-        await db.comment.create({
-          data: {
-            content: commentContent,
-            authorId: userId,
-            postId: i + 1,
-          },
+      console.log(createdUser);
+    }
+    for (let z = 1001; z < 1010; z++) {
+      for (let i = 1; i < 6; i++) {
+        await db.post.create({
+          data: createRandomPost(i * z, z),
         });
+        for (let j = 1; j < 21; j++) {
+          await db.comment.create({
+            data: createRandomComment(i * z, z),
+          });
+        }
       }
     }
   } catch (error) {
